ce9765bd7dbf01b8ff73e69c7ec740c2b07f0b4f
Euzebio <euzebiosantanawork@gmail.com>
2020-07-21 11:33:35 -0300

    version1.0

---
+++ math.js
@@ -0,0 +1,25 @@
+function soma ( a , b ) {
+
+    return a + b ;
+
+}
+
+function somaComposta ( a , b , c) {
+
+    return a + b + c ;
+
+}
+
+function multiplicacao ( a , b ) {
+
+    return a * b
+
+}
+
+function calc ( a , b , c ) {
+
+    return ( a + b ) * c ;
+
+}
+
+module.exports = { soma , somaComposta , multiplicacao, calc }
